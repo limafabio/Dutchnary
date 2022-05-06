529d78dd0146535072447add1d24a1b4a960ea99
limafabio <fabiolima39@gmail.com>
2022-05-05 22:28:54 -0300

    class Word refactor

--- Src/Word.cpp
+++ Src/Word.cpp
@@ -3,3 +3,44 @@
 //
 
 #include "Word.h"
+#include <string>
+
+    //Word::setter
+    void Word::setId(const int i) {
+        id = i;
+    }
+    void Word::setType(const std::string &t) {
+        type = t;
+    }
+    void Word::setMeaning(const std::string &m) {
+        meaning = m;
+    }
+    void Word::setSound(const std::string &s) {
+        sound = s;
+    }
+    void Word::setDutch(const std::string &d) {
+        dutch = d;
+    }
+    void Word::setPicture(char *p) {
+        picture = p;
+    }
+
+    //Getter
+    int Word::getId() {
+        return id;
+    }
+    std::string Word::getType() {
+        return type;
+    }
+    std::string Word::getMeaning() {
+        return meaning;
+    }
+    std::string Word::getSound() {
+        return sound;
+    }
+    std::string Word::getDutch() {
+        return dutch;
+    }
+    char * Word::getPicture() {
+        return picture;
+    }
--- Src/Word.h
+++ Src/Word.h
@@ -5,107 +5,29 @@
 #ifndef DUTCHNARY_WORD_H
 #define DUTCHNARY_WORD_H
 #include <string>
-#include <fstream>
-
-
 class Word {
+public:
+    int id;
     std::string type;
     std::string meaning;
     std::string sound;
     std::string dutch;
-    char * picture;
+    char *picture;
 
-public:
-    //Setter
-    void setType(const std::string & t) {
-        type = t;
-    }
-    void setMeaning(const std::string & m){
-        meaning = m;
-    }
+    void setId(const int i);
+    void setType(const std::string &t);
+    void setMeaning(const std::string &m);
+    void setSound(const std::string &s);
+    void setDutch(const std::string &d);
+    void setPicture(char *p);
 
-    void setSound(const std::string & s){
-        sound = s;
-    }
+    int getId();
+    std::string getType();
+    std::string getMeaning();
+    std::string getSound();
+    std::string getDutch();
+    char *getPicture();
 
-    void setDutch(const std::string &d){
-        dutch = d;
-    }
-
-    void setPicture(char * p){
-        picture = p;
-    }
-
-    //Getter
-    std::string getType(){
-        return type;
-    }
-
-    std::string getMeaning(){
-        return meaning;
-    }
-
-    std::string getSound(){
-        return sound;
-    }
-
-    std::string  getDutch(){
-        return dutch;
-    }
-
-    char * getPicture(){
-        return picture;
-    }
-
-    int readWord(std::string word){
-
-        std::string location_file = DataBase;
-        std::ifstream file(location_file);
-        if(file.is_open())
-        {
-            std::string line;
-            while(std::getline(file,line)){
-                printf("%s", line.c_str());
-            }
-            file.close();
-        }
-    return 0;
-    }
-
-    int writeWord(std::string word){
-        std::string location_file = DataBase;
-        std::fstream file;
-        file.open(location_file,std::ios_base::app);
-        if (file.is_open())
-            file.write(word.data(),word.size());
-        return 0;
-    }
-
-    bool isEqualWord(std::string word1, std::string word2){
-        if (word1 == word2)
-            return true;
-        else
-            return false;
-
-    }
-
-    int searchWord(std::string word){
-        std::string location_file = DataBase;
-        std::ifstream file(location_file);
-        if(file.is_open())
-        {
-            std::string line;
-            while(std::getline(file,line)){
-                if (isEqualWorld(line,word)){
-                  return 0;
-                }
-                printf("%s", line.c_str());
-            }
-            file.close();
-        }
-        return -1;
-    }
 };
-
 
 #endif //DUTCHNARY_WORD_H
