4e0fcaa9df096660512a1e785344c55350137c35
limafabio <fabiolima39@gmail.com>
2022-05-05 23:18:16 -0300

    class Dictionary create with methods read and write

---
+++ Src/Dictionary.cpp
@@ -0,0 +1,54 @@
+//
+// Created by fabio on 05/05/22.
+//
+
+#include<fstream>;
+#include "Dictionary.h";
+#include "Word.h";
+
+     std::vector<Word> Dictionary::readWords(){
+
+        std::string location_file = DataBase;
+        std::ifstream file(location_file);
+        if(file.is_open())
+        {
+            std::string line;
+            while(std::getline(file,line)){
+                //TODO convert lines in object word
+                printf("%s", line.c_str());
+            }
+            file.close();
+        }
+        return words;
+    };
+
+    int Dictionary::writeWords(std::vector<Word> words){
+
+        std::string location_file = DataBase;
+        std::fstream file;
+        file.open(location_file,std::ios_base::app);
+       //TODO convert word in line, search a method to salve in format that can export to database
+        if (file.is_open())
+
+        return 0;
+    };
+
+
+    Word Dictionary::searchDutchWord(std::string dutch){
+        //TODO create a standard error
+        if (null == words)
+            return null;
+
+        Word wordFound;
+        int sizeWords = words.size();
+        for(int i = 0; i < sizeWords; i++){
+            Word currentWord;
+            if (currentWord.getDutch() == dutch){
+                wordFound = currentWord;
+                break;
+            }
+        }
+
+        return wordFound;
+    }
+
---
+++ Src/Dictionary.h
@@ -0,0 +1,22 @@
+//
+// Created by fabio on 05/05/22.
+//
+
+#ifndef DUTCHNARY_DICTIONARY_H
+#define DUTCHNARY_DICTIONARY_H
+#include<vector>;
+#include"Word.h";
+
+class Dictionary {
+
+public:
+    std::vector<Word> words;
+
+    std::vector<Word> readWords();
+    int writeWords(std::vector<Word>);
+    Word searchDutchWord(std::string dutch);
+
+};
+
+
+#endif //DUTCHNARY_DICTIONARY_H
